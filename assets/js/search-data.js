6e2536c65709f4ca57b967455a4c2c7bb5c4e4be
Shihang-Wang-58 <53704527+Shihang-Wang-58@users.noreply.github.com>
2025-04-26 15:20:03 +0000

    Deploying to gh-pages from @ Shihang-Wang-58/Shihang-Wang-58.github.io@2d5b9ebee0963c787666089d11d6dcbbd68c3c29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Wish to make lots of good jobs! Welcome to talk with me!",
@@ -36,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
